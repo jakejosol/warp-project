0866ce344a5435d0c4c5f38d461a470ceb3c495a
Jake Josol <jmjosol@hotmail.com>
2014-11-15 22:11:29 +0800

    Fixed head control

--- references/controls/Head.php
+++ references/controls/Head.php
@@ -18,11 +18,11 @@ class Head extends Control
 	public function Bootstrap()
 	{
 		$this
-		->AddChild(Title::Create()->SetText(Application::GetInstance()->GetTitle() . " - " . App::GetInstance()->GetSubtitle()))
+		->AddChild(Title::Create()->SetText(App::Meta()->Title() . " - " . App::Meta()->Subtitle()))
 		->AddChild(Meta::Create()->SetProperty("http-equiv","X-UA-Compatible")->SetContent("IE=edge"))
 		->AddChild(Meta::Create()->SetName("charset")->SetContent("utf-8"))
 		->AddChild(Meta::Create()->SetName("viewport")->SetContent("width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"))
-		->AddChild(Meta::Create()->SetName("description")->SetContent(Application::GetInstance()->GetDescription()))
+		->AddChild(Meta::Create()->SetName("description")->SetContent(App::Meta()->Description()))
 		->AddChild(Text::Create(Resource::Render()));
 	}
 }
